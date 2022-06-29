aac3b15f28817ca1200549948abc820149c54613
BobbyKJH <rlatnguscjsw@gmail.com>
2022-06-29 20:10:16 +0900

    Docs: Edit eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,22 @@
 module.exports = {
-  env: {
-    jest: true,
-    browser: true,
-    es2021: true,
-  },
-  extends: ['airbnb'],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  plugins: ['react', 'prettier'],
-  rules: {
-    'no-console': 'off',
-    'no-unused-vars': 'off',
-    'react/prop-types': 0,
-  },
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: ['airbnb', 'prettier'],
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['react', 'prettier'],
+	rules: {
+		'no-console': 'off',
+		'no-unused-vars': 'off',
+		'react/prop-types': 0,
+		'linebreak-style': ['error', require('os').EOL === '\r\n' ? 'windows' : 'unix'],
+		'import/prefer-default-export': 'off',
+	},
 };
