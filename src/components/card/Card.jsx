7c016703ec61de8316e2a508676dbf903d3ba89c
BobbyKJH <rlatnguscjsw@gmail.com>
2022-07-04 16:34:07 +0900

    Feat: Card.jsx

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -5,33 +5,15 @@ function Card({ Img, Title, Content = '', Price, Group, Theme, Package }) {
 		<>
 			<img src={Img} alt={Title} />
 			<div>
-				<span>{Group}</span>
-				<span>{Theme}</span>
-				<span>{Package}</span>
-
-				<h1>
-					{Title.length > 35 ? (
-						<>
-							{Title.slice(0, 35)}
-							...
-						</>
-					) : (
-						<>{Title.slice(0, 34)}</>
-					)}
-				</h1>
-
-				<h2>
-					{Content.length > 35 ? (
-						<>
-							{Content.slice(0, 35)}
-							...
-						</>
-					) : (
-						<>{Content.slice(0, 34)}</>
-					)}
-				</h2>
-
-				<h3>{Price}원</h3>
+				<span className="hashTag">{Group}</span>
+				<span className="hashTag">{Theme}</span>
+				<span className="hashTag">{Package}</span>
+				{/* 제목 */}
+				<h1 className="title">{Title}</h1>
+				{/* 내용 */}
+				<h2 className="content">{Content}</h2>
+				{/* 가격 */}
+				<h3 className="price">{Price}원</h3>
 			</div>
 		</>
 	);
