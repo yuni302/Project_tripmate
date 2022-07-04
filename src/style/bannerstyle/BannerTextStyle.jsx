69c3b93c84c3f3a14c99c7e8a6aa323a031068b4
Hungeun2 <bepoohun@gmail.com>
2022-07-04 19:13:00 +0900

    Fix: delete unused code

--- src/style/bannerstyle/BannerTextStyle.jsx
+++ src/style/bannerstyle/BannerTextStyle.jsx
@@ -1,4 +1,4 @@
-import styled, { createGlobalStyle } from 'styled-components';
+import styled from 'styled-components';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
