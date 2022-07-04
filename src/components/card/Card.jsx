9bf7fdd366899222ab74a3efeb5bae76629172df
BobbyKJH <rlatnguscjsw@gmail.com>
2022-07-04 19:43:26 +0900

    Refactor: Card.jsx className

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -5,9 +5,9 @@ function Card({ Img, Title, Content = '', Price, Group, Theme, Package }) {
 		<>
 			<img src={Img} alt={Title} />
 			<div>
-				<span className="hashTag">{Group}</span>
-				<span className="hashTag">{Theme}</span>
-				<span className="hashTag">{Package}</span>
+				<span className="hash-tag">{Group}</span>
+				<span className="hash-tag">{Theme}</span>
+				<span className="hash-tag">{Package}</span>
 				{/* 제목 */}
 				<p className="title">{Title}</p>
 				{/* 내용 */}
