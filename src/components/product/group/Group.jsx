88249c2aeb540c278cae07a578333efe9170f533
yuni302 <zhyxxn@naver.com>
2022-07-05 17:11:04 +0900

    Refactor: Group Component

--- src/components/product/group/Group.jsx
+++ src/components/product/group/Group.jsx
@@ -4,80 +4,33 @@ import GroupFilter from './GroupFilter';
 
 function Group() {
 	const [content, setContent] = useState('2030');
+	const GroupList = [
+		['2030', '4050', '6070', '여자끼리', '남자끼리', '자녀동반'],
+		['2030끼리', '4050끼리', '6070끼리', '여자끼리', '남자끼리', '자녀동반'],
+	];
 
-	const btnValue = (e) => {
-		const {
-			target: { name },
-		} = e;
-		if (name === '2030') {
-			setContent('2030');
-		} else if (name === '4050') {
-			setContent('4050');
-		} else if (name === '6070') {
-			setContent('6070');
-		} else if (name === '여자끼리') {
-			setContent('여자끼리');
-		} else if (name === '남자끼리') {
-			setContent('남자끼리');
-		} else if (name === '자녀동반') {
-			setContent('자녀동반');
-		}
-	};
+	const GroupType = GroupList[0].map((data, index) => (
+		<button
+			type="button"
+			name={data}
+			className={index === 0 ? 'group-btn active' : 'group-btn'}
+			onClick={(e) => {
+				setContent(e.target.name);
+				for (let i = 0; i < GroupList[0].length; i += 1) {
+					e.target.parentNode.childNodes[i].className = 'group-btn';
+				}
+				e.target.classList.add('active');
+			}}
+		>
+			{GroupList[1][index]}
+		</button>
+	));
 
 	return (
 		<>
 			<MainTitle>고투게더가 추천하는 그룹별 상품이에요</MainTitle>
 			<Box>
-				<ButtonGroup>
-					<button
-						type="button"
-						onClick={btnValue}
-						style={{ background: content === '2030' && '#0080C6', color: content === '2030' && '#ffffff' }}
-						name="2030"
-					>
-						2030끼리
-					</button>
-					<button
-						type="button"
-						onClick={btnValue}
-						style={{ background: content === '4050' && '#0080C6', color: content === '4050' && '#ffffff' }}
-						name="4050"
-					>
-						4050끼리
-					</button>
-					<button
-						type="button"
-						onClick={btnValue}
-						style={{ background: content === '6070' && '#0080C6', color: content === '6070' && '#ffffff' }}
-						name="6070"
-					>
-						6070끼리
-					</button>
-					<button
-						type="button"
-						onClick={btnValue}
-						style={{ background: content === '여자끼리' && '#0080C6', color: content === '여자끼리' && '#ffffff' }}
-						name="여자끼리"
-					>
-						여자끼리
-					</button>
-					<button
-						type="button"
-						onClick={btnValue}
-						style={{ background: content === '남자끼리' && '#0080C6', color: content === '남자끼리' && '#ffffff' }}
-						name="남자끼리"
-					>
-						남자끼리
-					</button>
-					<button
-						type="button"
-						onClick={btnValue}
-						style={{ background: content === '자녀동반' && '#0080C6', color: content === '자녀동반' && '#ffffff' }}
-						name="자녀동반"
-					>
-						자녀동반
-					</button>
-				</ButtonGroup>
+				<ButtonGroup>{GroupType}</ButtonGroup>
 				<CardList>
 					<GroupFilter content={content} />
 				</CardList>
