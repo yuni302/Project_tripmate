d811188cfa376cfb1f60c163204d847940f8c26f
BobbyKJH <rlatnguscjsw@gmail.com>
2022-07-08 14:33:59 +0900

    Feat: Router Connection Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import MainPage from './page/MainPage';
+// components
+import Header from './components/Header';
+import Footer from './components/Footer';
+// page
+import MainPage from './page/main/MainPage';
+import SearchPage from './page/search/SearchPage';
+import ListPage from './page/list/ListPage';
+import DetailPage from './page/detail/DetailPage';
+import LogInPage from './page/sign/LogInPage';
+import SignUpPage from './page/sign/SignUpPage';
+import MyPage from './page/mypage/MyPage';
+import KeywordPage from './page/keyword/KeywordPage';
 
 function App() {
 	return (
-		<div>
+		<>
+			<Header />
+
 			<Routes>
 				<Route path="/" element={<MainPage />} />
+				<Route path="/search" element={<SearchPage />} />
+				<Route path="/list" element={<ListPage />} />
+				<Route path="/list/:id" element={<DetailPage />} />
+				<Route path="/login" element={<LogInPage />} />
+				<Route path="/signup" element={<SignUpPage />} />
+				<Route path="/keyword" element={<KeywordPage />} />
+				<Route path="/mypage" element={<MyPage />} />
 			</Routes>
-		</div>
+
+			<Footer />
+		</>
 	);
 }
 
