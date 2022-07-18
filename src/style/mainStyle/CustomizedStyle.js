6107aac899258e0f9eaabaec638aa642a79e6b2b
sumin10 <umsumin10@gmail.com>
2022-07-18 15:00:44 +0900

    Style: Refactor CustomizedStyle

--- src/style/mainStyle/CustomizedStyle.js
+++ src/style/mainStyle/CustomizedStyle.js
@@ -1,18 +1,18 @@
 import styled from 'styled-components';
 
-export const CustomizedTitle = styled.p`
-  // width: 100%;
-  max-width: 1279px;
-  font-family: 'Pretendard';
-  color: #000;
-  font-weight: 700;
-  font-size: 26px;
-  line-height: 31px;
+export const CustomizedTitle = styled.div`
+  width: 1279px;
   margin: 0 auto 30px;
+  p {
+    color: #000;
+    font-weight: 700;
+    font-size: 26px;
+    line-height: 31px;
+  }
 `;
 
 export const Box = styled.div`
-  max-width: 1440px;
+  width: 1440px;
   display: flex;
   flex-flow: row wrap;
   justify-content: center;
