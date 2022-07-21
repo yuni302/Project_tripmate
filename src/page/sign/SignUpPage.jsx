05bcbc181f1e3309ced0eddb78c40785e05ad574
BobbyKJH <rlatnguscjsw@gmail.com>
2022-07-21 14:28:31 +0900

    Feat: SignUpPage create

--- src/page/sign/SignUpPage.jsx
+++ src/page/sign/SignUpPage.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import SignUpBox from 'components/page/sign/SignUpBox';
 
-const SignUpPage = () => <div>SignUpPage</div>;
+const SignUpPage = () => (
+  <div>
+    <SignUpBox />
+  </div>
+);
 
 export default SignUpPage;
