1a0cef873cef79dbc9244655e1111a90e4f28125
sumin10 <umsumin10@gmail.com>
2022-07-21 16:49:54 +0900

    Refactor: Add PageNation ThemeListPage

--- src/page/list/ThemeListPage.jsx
+++ src/page/list/ThemeListPage.jsx
@@ -1,9 +1,11 @@
+import PageNation from 'components/page/list/PageNation';
 import ThemeList from 'components/page/list/ThemeList';
 import React from 'react';
 
 const ThemeListPage = () => (
   <div>
     <ThemeList />
+    <PageNation />
   </div>
 );
 
