d1852559a50bb6f0497ce801136bdcb298dc0ed3
sumin10 <umsumin10@gmail.com>
2022-07-21 16:49:03 +0900

    Refactor: Add PageNation RecommendedListPage

--- src/page/list/RecommendedListPage.jsx
+++ src/page/list/RecommendedListPage.jsx
@@ -1,9 +1,11 @@
 import CustomizedList from 'components/page/list/CustomizedList';
+import PageNation from 'components/page/list/PageNation';
 import React from 'react';
 
 const RecommendedListPage = () => (
   <div>
     <CustomizedList />
+    <PageNation />
   </div>
 );
 
