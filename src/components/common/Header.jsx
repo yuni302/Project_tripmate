9dd9ca4d1da2b6945be9c389d7677fd7a109f230
BobbyKJH <rlatnguscjsw@gmail.com>
2022-07-22 14:40:18 +0900

    Refactor: ScrollY 141 change

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -19,7 +19,7 @@ const Header = () => {
 
   // 스크롤 위치에 따른 position 변경
   const handleScroll = () => {
-    if (scroll > 118) {
+    if (scroll > 141) {
       setScroll(window.pageYOffset);
       setDistance(false);
     } else {
@@ -83,101 +83,103 @@ const Header = () => {
 
           <hr />
 
-          <header id="header">
-            <h1>
-              <Link to="/">
-                <HeaderLogo />
-              </Link>
-            </h1>
-            <ul className="content">
-              <Link to="/recommendedlist">
-                <span>추천 여행 상품</span>
-              </Link>
-            </ul>
-            <ul className="content">
-              <Link to="/grouplist" state={{ group: '2030' }}>
-                <span>그룹별 여행</span>
-              </Link>
-              <Link to="/grouplist" state={{ group: '2030' }}>
-                <nav>2030끼리</nav>
-              </Link>
-              <Link to="/grouplist" state={{ group: '4050' }}>
-                <nav>4050끼리</nav>
-              </Link>
-              <Link to="/grouplist" state={{ group: '6070' }}>
-                <nav>6070끼리</nav>
-              </Link>
-              <Link to="/grouplist" state={{ group: 'female' }}>
-                <nav>여자끼리</nav>
-              </Link>
-              <Link to="/grouplist" state={{ group: 'male' }}>
-                <nav>남자끼리</nav>
-              </Link>
-              <Link to="/grouplist" state={{ group: 'family' }}>
-                <nav>자녀동반</nav>
-              </Link>
-            </ul>
-            <ul className="content">
-              <Link to="/themelist" state={{ theme: 'culture' }}>
-                <span>테마별 여행</span>
-              </Link>
-              <Link to="/themelist" state={{ theme: 'culture' }}>
-                <nav>문화탐방</nav>
-              </Link>
-              <Link to="/themelist" state={{ theme: 'rest' }}>
-                <nav>휴양지</nav>
-              </Link>
-              <Link to="/themelist" state={{ theme: 'golf' }}>
-                <nav>골프여행</nav>
-              </Link>
-              <Link to="/themelist" state={{ theme: 'trek' }}>
-                <nav>트레킹</nav>
-              </Link>
-              <Link to="/themelist" state={{ theme: 'circuit' }}>
-                <nav>성지순례</nav>
-              </Link>
-            </ul>
-            <ul className="content">
-              <Link to="/arealist" state={{ area: 'sea' }}>
-                <span>지역별 여행</span>
-              </Link>
-              <Link to="/arealist" state={{ area: 'sea' }}>
-                <nav>동남아시아</nav>
-              </Link>
-              <Link to="/arealist" state={{ area: 'nea' }}>
-                <nav>동북-중앙아시아</nav>
-              </Link>
-              <Link to="/arealist" state={{ area: 'pacific' }}>
-                <nav>서남아시아-태평양</nav>
-              </Link>
-              <Link to="/arealist" state={{ area: 'eu' }}>
-                <nav>유럽-코카서스</nav>
-              </Link>
-              <Link to="/arealist" state={{ area: 'america' }}>
-                <nav>북미-중남미</nav>
-              </Link>
-              <Link to="/arealist" state={{ area: 'africa' }}>
-                <nav>아프리카-중동</nav>
-              </Link>
-            </ul>
-            <ul className="content info">
-              <Link to="/information" state={{ infor: 'notice' }}>
-                <span>고객 센터</span>
-              </Link>
-              <Link to="/information" state={{ infor: 'notice' }}>
-                <nav>공지사항</nav>
-              </Link>
-              <Link to="/information" state={{ infor: 'frequent' }}>
-                <nav>자주 묻는 질문</nav>
-              </Link>
-              <Link to="/information " state={{ infor: 'oneonone' }}>
-                <nav>1:1 문의</nav>
-              </Link>
-              <Link to="/information" state={{ infor: 'group-ask' }}>
-                <nav>단체 문의</nav>
-              </Link>
-            </ul>
-          </header>
+          <div className="header-max">
+            <header id="header">
+              <h1>
+                <Link to="/">
+                  <HeaderLogo />
+                </Link>
+              </h1>
+              <ul className="content">
+                <Link to="/recommendedlist">
+                  <span>추천 여행 상품</span>
+                </Link>
+              </ul>
+              <ul className="content">
+                <Link to="/grouplist" state={{ group: '2030' }}>
+                  <span>그룹별 여행</span>
+                </Link>
+                <Link to="/grouplist" state={{ group: '2030' }}>
+                  <nav>2030끼리</nav>
+                </Link>
+                <Link to="/grouplist" state={{ group: '4050' }}>
+                  <nav>4050끼리</nav>
+                </Link>
+                <Link to="/grouplist" state={{ group: '6070' }}>
+                  <nav>6070끼리</nav>
+                </Link>
+                <Link to="/grouplist" state={{ group: 'female' }}>
+                  <nav>여자끼리</nav>
+                </Link>
+                <Link to="/grouplist" state={{ group: 'male' }}>
+                  <nav>남자끼리</nav>
+                </Link>
+                <Link to="/grouplist" state={{ group: 'family' }}>
+                  <nav>자녀동반</nav>
+                </Link>
+              </ul>
+              <ul className="content">
+                <Link to="/themelist" state={{ theme: 'culture' }}>
+                  <span>테마별 여행</span>
+                </Link>
+                <Link to="/themelist" state={{ theme: 'culture' }}>
+                  <nav>문화탐방</nav>
+                </Link>
+                <Link to="/themelist" state={{ theme: 'rest' }}>
+                  <nav>휴양지</nav>
+                </Link>
+                <Link to="/themelist" state={{ theme: 'golf' }}>
+                  <nav>골프여행</nav>
+                </Link>
+                <Link to="/themelist" state={{ theme: 'trek' }}>
+                  <nav>트레킹</nav>
+                </Link>
+                <Link to="/themelist" state={{ theme: 'circuit' }}>
+                  <nav>성지순례</nav>
+                </Link>
+              </ul>
+              <ul className="content">
+                <Link to="/arealist" state={{ area: 'sea' }}>
+                  <span>지역별 여행</span>
+                </Link>
+                <Link to="/arealist" state={{ area: 'sea' }}>
+                  <nav>동남아시아</nav>
+                </Link>
+                <Link to="/arealist" state={{ area: 'nea' }}>
+                  <nav>동북-중앙아시아</nav>
+                </Link>
+                <Link to="/arealist" state={{ area: 'pacific' }}>
+                  <nav>서남아시아-태평양</nav>
+                </Link>
+                <Link to="/arealist" state={{ area: 'eu' }}>
+                  <nav>유럽-코카서스</nav>
+                </Link>
+                <Link to="/arealist" state={{ area: 'america' }}>
+                  <nav>북미-중남미</nav>
+                </Link>
+                <Link to="/arealist" state={{ area: 'africa' }}>
+                  <nav>아프리카-중동</nav>
+                </Link>
+              </ul>
+              <ul className="content info">
+                <Link to="/information" state={{ infor: 'notice' }}>
+                  <span>고객 센터</span>
+                </Link>
+                <Link to="/information" state={{ infor: 'notice' }}>
+                  <nav>공지사항</nav>
+                </Link>
+                <Link to="/information" state={{ infor: 'frequent' }}>
+                  <nav>자주 묻는 질문</nav>
+                </Link>
+                <Link to="/information " state={{ infor: 'oneonone' }}>
+                  <nav>1:1 문의</nav>
+                </Link>
+                <Link to="/information" state={{ infor: 'group-ask' }}>
+                  <nav>단체 문의</nav>
+                </Link>
+              </ul>
+            </header>
+          </div>
         </HeaderStyle>
       ) : (
         <MobileHeader />
